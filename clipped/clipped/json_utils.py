3c75d5eaa614e01b719ff540790fbebe0bde4190
mouradmourafiq <mouradmourafiq@gmail.com>
2023-04-12 15:54:41 +0200

    Fix lint issue

--- clipped/clipped/json_utils.py
+++ clipped/clipped/json_utils.py
@@ -10,7 +10,10 @@ def orjson_dumps(
 ) -> str:
     return orjson.dumps(obj, default=default).decode()
 
-def orjson_dumps_with_options(v: Any, *, default: Optional[Callable[[Any], Any]] = None) -> str:
+
+def orjson_dumps_with_options(
+    v: Any, *, default: Optional[Callable[[Any], Any]] = None
+) -> str:
     return orjson.dumps(
         v, default=default, option=orjson.OPT_NON_STR_KEYS | orjson.OPT_SERIALIZE_NUMPY
     ).decode()
