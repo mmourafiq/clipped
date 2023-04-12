60b91e5bec1115c3339022154efc2925e834f1c4
mouradmourafiq <mouradmourafiq@gmail.com>
2023-04-12 21:56:28 +0200

    Bump version to v0.0.5
     * Expose option on orjson
     * Set numpy handling as default
     * Refactor layout

--- clipped/clipped/json_utils.py
+++ clipped/clipped/json_utils.py
@@ -6,14 +6,7 @@ import orjson
 def orjson_dumps(
     obj: Any,
     *,
+    option: Optional[int] = orjson.OPT_SERIALIZE_NUMPY,
     default: Optional[Callable[[Any], Any]] = None,
 ) -> str:
-    return orjson.dumps(obj, default=default).decode()
-
-
-def orjson_dumps_with_options(
-    v: Any, *, default: Optional[Callable[[Any], Any]] = None
-) -> str:
-    return orjson.dumps(
-        v, default=default, option=orjson.OPT_NON_STR_KEYS | orjson.OPT_SERIALIZE_NUMPY
-    ).decode()
+    return orjson.dumps(obj, default=default, option=option).decode()
--- clipped/clipped/pkg.py
+++ clipped/clipped/pkg.py
@@ -1,5 +1,5 @@
 NAME = "clipped"
-VERSION = "0.0.4"
+VERSION = "0.0.5"
 DESC = "Common shortcuts and utils."
 URL = "https://github.com/mmourafiq/clipped"
 AUTHOR = "Mourad Mourafiq"
